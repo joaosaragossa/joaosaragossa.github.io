3cdcd3f273d98a47b6ebb0fc3c831fab4606ee4f
João Paulo Saragossa <joaosaragossa.dev@gmail.com>
2014-09-21 18:42:50 -0300

    Criado e editado header, sec-quemsou, criado sec-oquefaco, implementado jquery features, imagens

---
+++ script.js
@@ -0,0 +1,27 @@
+$(document).ready(function()
+{
+
+
+  $('#menu ul li a').click(function()
+    {
+      $('html, body').animate({scrollTop: $($(this).attr("href")).offset().top}, 2000);
+      return false;
+    });
+
+
+  $('.parallax').each(function()
+  {
+    var $obj = $(this);
+   
+    $(window).scroll(function() 
+    {
+      var yPos = -($(window).scrollTop() / $obj.data('speed')); 
+   
+      var bgpos = '50% '+ yPos + 'px';
+   
+      $obj.css('background-position', bgpos );
+      return false;
+    });
+    return false;
+  });
+});
